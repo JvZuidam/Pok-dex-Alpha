77278e1fce5734a931b45a40a7e08ed97767d61c
Jim van Zuidam <Jim van Zuidam>
2016-09-19 21:35:21 +0200

    Gathering users entered data and valadating it. Created a connection to the database

---
+++ Pokèdex Alpha/Pokèdex Alpha/Classes/DbConnect.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Media.Animation;
+
+namespace Pokèdex_Alpha.Classes
+{
+    class DbConnect
+    {
+        public static string Connect()
+        {
+            // Create the connection to the resource!
+            // This is the connection, that is established and
+            // will be available throughout this block.
+            using (SqlConnection conn = new SqlConnection())
+            {
+                // Create the connectionString
+                // Trusted_Connection is used to denote the connection uses Windows Authentication
+                conn.ConnectionString = "Server=u754486136_users;Database=u754486136_users;Trusted_Connection=true";
+                conn.Open();
+
+                // Create the command, to insert the data into the Table!
+                // this is a simple INSERT INTO command!
+
+                SqlCommand insertCommand =new SqlCommand("INSERT INTO Users (Name, Username, Password, Salt, Email, Confirm) VALUES (@0, @1, @2, @3, @4, @5)", conn);
+
+                // In the command, there are some parameters denoted by @, you can 
+                // change their value on a condition, in my code they're hardcoded.
+
+                insertCommand.Parameters.Add(new SqlParameter("0", "Jim"));
+                insertCommand.Parameters.Add(new SqlParameter("1", "Swift_Gale"));
+                insertCommand.Parameters.Add(new SqlParameter("2", "Henk"));
+                insertCommand.Parameters.Add(new SqlParameter("3", "dwadjwadwkdpow"));
+                insertCommand.Parameters.Add(new SqlParameter("4", "jimvanzuidam@hotmail.nl"));
+                insertCommand.Parameters.Add(new SqlParameter("5", 0));
+
+                return "succes";
+
+            }
+        }
+    }
+}
--- Pokèdex Alpha/Pokèdex Alpha/Register.xaml.cs
+++ Pokèdex Alpha/Pokèdex Alpha/Register.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +13,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Pokèdex_Alpha.Classes;
+
 
 namespace Pokèdex_Alpha
 {
@@ -23,10 +27,76 @@ namespace Pokèdex_Alpha
         {
             InitializeComponent();
         }
+        
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            
+            //Create variables for the textbox elements
+            var name = TxtNm.Text;
+            var username = TxtUsrNm.Text;
+            var password = TxtPswd.Password;
+            var passwordRepeat = TxtPswdRpt.Password;
+            var email = TxtEml.Text;
+
+            //Run a bunch of if statements to find out if the user has not filled out one or more of the forms.
+            //Return a messagebox telling the user which form(s) they still have to fill in.
+            //Following the order of: Name, Username, Email, Password, repeated password.
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                MessageBox.Show(this, "You did not enter a name" + (string.IsNullOrWhiteSpace(username) ? " and username" : "") + (string.IsNullOrWhiteSpace(email) ? " and email" : "") + (string.IsNullOrWhiteSpace(password) ? " and password" : "") + (string.IsNullOrWhiteSpace(passwordRepeat) ? " and the repeated password" : ""));
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(username))
+            {
+                MessageBox.Show(this, "You did not enter a username" + (string.IsNullOrWhiteSpace(email) ? " and email" : "") + (string.IsNullOrWhiteSpace(password) ? " and password" : "") + (string.IsNullOrWhiteSpace(passwordRepeat) ? " and the repeated password" : ""));
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(email))
+            {
+                MessageBox.Show(this, "You did not enter an email" + (string.IsNullOrWhiteSpace(password) ? " and password" : "") + (string.IsNullOrWhiteSpace(passwordRepeat) ? " and the repeated password" : ""));
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show(this, "You did not enter a password" + (string.IsNullOrWhiteSpace(passwordRepeat) ? " and the repeated password" : ""));
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(passwordRepeat))
+            {
+                MessageBox.Show(this, "You did not enter the repeated password");
+                return;
+            }
+            //When all froms are filled run a few validation checks
+            //Hash and salt the password
+            //Send all the data to the database
+            else
+            {
+                //Check with the help of the email validation class if the user entered a valid email address.
+                EmailValidation Email = new EmailValidation();
+                if (Email.IsValidEmail(email))
+                {
+                    Console.WriteLine("Valid");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid");
+                }
+
+                // Check if the password and repeated password are the same
+                if (password == passwordRepeat)
+                {
+                    Console.WriteLine("Same");
+                }
+                else
+                {
+                    Console.WriteLine("Different");
+                }
+                //Hash and salt the entered password.
+                var Hash = Hashing.CreateHash(password);
+                Console.WriteLine(Hash);
+            }
+            Console.WriteLine("Succesful");
+            DbConnect.Connect();
         }
     }
 }
